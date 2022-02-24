37731c1d6b0077b77885c541e570d024365ef8b0
Mizunomori <hhh58@cornell.edu>
2022-02-24 12:58:54 -0500

    Files

--- HW2_Code.py
+++ HW2_Code.py
@@ -3,6 +3,8 @@ import streamlit as st
 import numpy as np
 import time 
 import pandas as pd 
+import csv as csv 
+
 
 st.title("SYSEN 5160 HW 2")
 
@@ -10,4 +12,13 @@ st.subheader("Analytical Hierarchy Process")
 
 st.text("Enter the Matrix with the Attributes Ranked")
 
-st.file_uploader("Attributes Pairwise Comparison")
+att_file = st.file_uploader("Attributes Pairwise Comparison CSV File") 
+
+att_array = csv.reader(att_file)
+
+st.table(att_array)
+
+
+st.text("Pairwise Comparisons of Options Added Here")
+
+opt = st.file_uploader("Options Pairwise Comparison") 
